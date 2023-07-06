30dfe67284671d5e3228a680471e6f7c6c17217a
Pedro Viana <pedroviana.developer13@gmail.com>
2023-07-06 14:40:52 -0300

    feat: number of students in the same time

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,19 @@
+"""
+Liner
+"""
+
+
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    """
+    Liner
+    """
+    counter = 0
+
+    try:
+        for period in permanence_period:
+            if period[0] <= target_time <= period[1]:
+                counter += 1
+    except TypeError:
+        return None
+
+    return counter
