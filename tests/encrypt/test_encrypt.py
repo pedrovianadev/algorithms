1b2f7b496c89e26a57d5b78fe0027eafd6e5ce67
Pedro Viana <pedroviana.developer13@gmail.com>
2023-07-06 14:52:31 -0300

    test: Inversion cryptography

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,23 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+""" a"""
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    """ a"""
+
+    # Test for even key
+    assert encrypt_message("message", 4) == "ega_ssem"
+
+    # Test for odd key
+    assert encrypt_message("message", 3) == "sem_egas"
+
+    # Test for invalid key
+    assert encrypt_message("message", -2) == "egassem"
+
+    # Test for incorrect input type
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(5, 3)
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("trybe", 3.9)
